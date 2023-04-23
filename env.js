068a3da46f8a27ff69c7a17799153c6dd11d8229
Eliott Moulin <fr.moulin37@gmail.com>
2023-04-23 19:08:32 +0200

    Push environment

--- env.js
+++ env.js
@@ -1,11 +1,11 @@
 // Générer une clé API sur https://openweathermap.org/current
-const WEATHER_API_KEY='6af99d5b8dc132bd2764b7920bb5cfd9';
+const WEATHER_API_KEY='XXX';
 
 // Générer une clé API sur https://www.themoviedb.org/settings/api
-const MOVIES_API_KEY='251b4d648eb3932f3a3bd8727c3473e8';
+const MOVIES_API_KEY='XXX';
 
 // Générer une clé API sur https://developers.giphy.com/dashboard/
-const GIPHY_API_KEY='rLYVXYBFz3KB7n1L3qfdnRGFvggdMp0a';
+const GIPHY_API_KEY='XXX';
 
 // Générer une clé API sur https://platform.openai.com/account/api-keys
-const CHATPGT_API_KEY='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';
+const CHATGPT_API_KEY='XXX';
